7bfb5216ccb88ea18901e7b6f41d3a4c6d516a44
sifeddineEddr <sifeddine.edr@gmail.com>
2024-03-10 02:29:38 +0100

    footer fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,17 +4,15 @@ import { footerData } from "../../assets/siteData";
 function Footer() {
   return (
     <footer className="bg-[#F8F9FA] py-8">
-      <div className="max-w-screen-xl flex  justify-between mx-auto p-4">
+      <div className="max-w-screen-xl flex justify-around mx-auto">
         <div className="flex flex-col gap-2">
           <img src="/logo.svg" alt="" />
           <p className="text-xs">© 2024. All rights reserved</p>
         </div>
         <div className="flex flex-row gap-x-20 ">
-          {
-            footerData.map((e)=>(
-              <FooterColumns {...e}/>
-            ))
-          }
+          {footerData.map((e) => (
+            <FooterColumns {...e} />
+          ))}
         </div>
       </div>
     </footer>
